847fb04a6fa6a0f53402ad7f8ad7f5d9573e081e
Colin Mills <king.lem@gmail.com>
2021-07-11 15:27:21 -0600

    Changed to 2D, some adjustments to the test window size and resolution.
    Essentially, got done with lesson 3 of the "Godot Action RPG Series."

--- Player_Controller.gd
+++ Player_Controller.gd
@@ -1,33 +1,34 @@
-extends KinematicBody
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+const ACCELERATION = 500
+const FRICTION = 600
+const MAX_SPEED = 80
 
+var velocity = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-export (int) var speed = 15
-
-var velocity = Vector3()
-
-func get_input():
-	velocity = Vector3()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.z += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.z -= 1
-	velocity = velocity.normalized() * speed
-
 func _physics_process(delta):
-	get_input()
+	var input_vector = Vector2()
+	
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+	input_vector = input_vector.normalized()
+
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+
+	print(velocity)
 	velocity = move_and_slide(velocity)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
